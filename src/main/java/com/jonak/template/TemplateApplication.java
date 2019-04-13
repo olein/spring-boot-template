d9b7de6138a841cc91f2c6dc587ccb20e3f5296b
Fahim Fahad <fahimfahad92@gmail.com>
2019-04-13 12:29:49 +0600

    version updated to 12

--- src/main/java/com/jonak/template/TemplateApplication.java
+++ src/main/java/com/jonak/template/TemplateApplication.java
@@ -9,10 +9,10 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan("com.jonak.template")
-//@EnableJpaRepositories
+@EnableJpaRepositories
 public class TemplateApplication extends SpringBootServletInitializer {
 
 	@Override
--- src/main/java/com/jonak/template/endpoint/PersonEndpoint.java
+++ src/main/java/com/jonak/template/endpoint/PersonEndpoint.java
@@ -52,6 +52,6 @@ public class PersonEndpoint {
             produces = "application/json")
     public @ResponseBody
     String createContact() {
-        return "template project 1.0.11";
+        return "template project 1.0.12";
     }
 }
