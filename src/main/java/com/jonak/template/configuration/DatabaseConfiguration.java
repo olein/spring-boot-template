d5cde9deed72f6341a9ba21d1bed91bfa566e73e
Fahim Fahad <fahimfahad92@gmail.com>
2019-04-13 12:20:13 +0600

    version updated to 8

--- src/main/java/com/jonak/template/configuration/DatabaseConfiguration.java
+++ src/main/java/com/jonak/template/configuration/DatabaseConfiguration.java
@@ -6,17 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.beans.PropertyVetoException;
-import java.util.Properties;
 
 @Configuration
-@EnableTransactionManagement
+//@EnableTransactionManagement
 public class DatabaseConfiguration {
 
     @Autowired
